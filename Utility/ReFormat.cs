edfb17e42e16c0b605492ad91e90ab3e5355fe70
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-19 18:09:40 +0100

    Add utility ReFormat for formating string phone to int

---
+++ Utility/ReFormat.cs
@@ -0,0 +1,18 @@
+﻿namespace CSVQ.Utility;
+
+public class ReFormat
+{
+    public static int Phone_To_Num(string num)
+    {
+        num = num.Replace("-", "");
+        try
+        {
+            int result=Convert.ToInt32(num);
+            return result;
+        }
+        catch
+        {
+            return -1;
+        }
+    }
+}
