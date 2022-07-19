92f3af67fd60a106909e3e63ee882c8bc155788a
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-19 18:26:51 +0100

    Add utility for displaying list in console

---
+++ Utility/Display.cs
@@ -0,0 +1,9 @@
+﻿namespace CSVQ.Utility;
+
+public class Display
+{
+    public void Display_in_Console(List<Enum> lists)
+    {
+        lists.ForEach(i => Console.WriteLine(i.ToString()));
+    }
+}
