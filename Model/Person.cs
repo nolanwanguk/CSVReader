1e88009d7c82ea2846fc6956cb85a51611e264e0
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-19 18:08:51 +0100

    Add record class People

---
+++ Model/Person.cs
@@ -0,0 +1,11 @@
+﻿using CSVQ.Utility;
+
+namespace CSVQ.Model;
+
+public record Person(int Id, string FirstName, string LastName, string Company, string Address, string City,
+    string County, string Postal, string Phone1, string Phone2, string Email, string Web)
+{
+    public int[] PhoneNumbers { get; init; } =
+        new int[2] { ReFormat.Phone_To_Num(Phone1), ReFormat.Phone_To_Num(Phone2) };
+    
+};
