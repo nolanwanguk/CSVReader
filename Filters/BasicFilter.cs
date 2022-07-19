3cd3cc561b1c5f5cc770694eb9a7f0d107fdd71a
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-19 18:25:41 +0100

    Add basic abstract classes for filter

--- Filters/BasicFilter.cs
+++ Filters/BasicFilter.cs
@@ -1,6 +1,10 @@
 ﻿namespace CSVQ.Filters;
 
-public class BasicFilter
+public abstract class BasicFilter
 {
-    
+    public Enum Results=default!;
+    public int Counts = default!;
+    public abstract Enum Transform(Enum lists);
+    public abstract Enum FilterFunc(Enum lists);
+    public abstract Enum Cleanup(Enum lists);
 }
