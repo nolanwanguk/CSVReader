faf7d0bf6a3e7f290801084122b713cc4aae1b7b
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-20 14:21:34 +0100

    remove Id column

--- IO/CSVReader.cs
+++ IO/CSVReader.cs
@@ -23,7 +23,7 @@ public class CsvReader : BasicReader
         {
             if (i == 0) continue;
             var row = CsvParser.Decouple(_content[i]);
-            Qresults.Add(new Person(i, row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9],
+            Qresults.Add(new Person(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9],
                 row[10]));
         }
     }
