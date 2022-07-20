aff8df6c1fabf3e3f74a7e93a3f4a3c1831ec48a
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-20 09:55:53 +0100

    add csvhelper reader

---
+++ IO/CSVHelperReader.cs
@@ -0,0 +1,29 @@
+﻿using CSVQ.Model;
+using CSVQ.Utility;
+using CsvHelper;
+using System;
+using System.Globalization;
+
+namespace CSVQ.IO;
+
+public class CSVHelperReader:BasicReader
+{
+    public List<Person> Qresults = new List<Person>();
+    public override void BeforeRead() { }
+    public override void Reader(string path)
+    {
+        using (var reader = new StreamReader(path))
+        using (var csv = new CsvHelper.CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            csv.Read();
+            csv.ReadHeader();
+            while (csv.Read())
+            {
+                var record = csv.GetRecord<Person>();
+                Qresults.Add(record);
+            }
+        }
+    }
+    public override void AfterRead(){}
+    
+}
