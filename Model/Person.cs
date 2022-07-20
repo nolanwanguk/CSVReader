c4f66773816beb267dbf343a65631244a8dae6ec
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-20 12:44:15 +0100

    update person model to fits csvhelper params

--- Model/Person.cs
+++ Model/Person.cs
@@ -1,10 +1,22 @@
-﻿using CSVQ.Utility;
+﻿using System.Globalization;
+using CSVQ.Filters;
+using System.Text;
+using CSVQ.Utility;
 
 namespace CSVQ.Model;
 
-public record Person(int Id, string FirstName, string LastName, string Company, string Address, string City,
-    string County, string Postal, string Phone1, string Phone2, string Email, string Web)
+public record Person(string first_name, string last_name, string company_name, string address, string city,
+    string county, string postal, string phone1, string phone2, string email, string web)
 {
-    public int[] PhoneNumbers { get; init; } =
-        new int[2] { ReFormat.Phone_To_Num(Phone1), ReFormat.Phone_To_Num(Phone2) };
+    internal string FirstName = first_name;
+    internal string LastName = last_name;
+    internal string Company = company_name;
+    internal string Address = address;
+    internal string City = city;
+    internal string County = county;
+    internal string Postal = postal;
+    internal string Email = email;
+    internal string Web = web;
+    internal int Phone1 = ReFormat.Phone_To_Num(phone1);
+    internal int Phone2 = ReFormat.Phone_To_Num(phone2);
 }
