794af60ab55cb1294d45eadb89f7e99786d51862
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-20 09:53:57 +0100

    update columns in basicreader

--- IO/BasicReader.cs
+++ IO/BasicReader.cs
@@ -2,9 +2,7 @@
 
 public abstract class BasicReader
 {
-    public string Path = default!;
     public abstract void BeforeRead();
-    public abstract Enum Reader(string path);
+    public abstract void Reader(string path);
     public abstract void AfterRead();
-
 }
