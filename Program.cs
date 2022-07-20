2f4d7286584cbe29a33396ef438cfd3a54ff9962
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-20 12:45:23 +0100

    add simple main entry

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using CSVQ.IO;
+using CSVQ.Utility;
+using CSVQ.Model;
+using CSVQ.Filters;
+
+namespace CSVQ
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Start");
+            var reader = new CSVHelperReader();
+            reader.Reader(@"Data/input.csv");
+            var results = reader.Qresults;
+            var filter = new PersonHouseNumberEquals3();
+            var f = filter.FilterFunc(results);
+            Console.WriteLine("Counts:{0}",f.Count);
+            Display.Display_in_Console_Debug(f);
+        }
+    }
+}
+
