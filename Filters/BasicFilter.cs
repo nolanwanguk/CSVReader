13b3afb2dff3bcd8d7eeb93d1cee157ad4e487be
nolanwanguk <nolan.w.sf@gmail.com>
2022-07-20 14:18:45 +0100

    add default filter for testing

--- Filters/BasicFilter.cs
+++ Filters/BasicFilter.cs
@@ -6,7 +6,28 @@ public interface IBasicFilter
 {
     public List<Person>? Results { get; set; }
     public int Counts { get; set; }
-    public List<Person>? Transform(List<Person> list);
+    public List<Person>? Transform(List<Person> lists);
     public List<Person>? FilterFunc(List<Person> lists);
     public List<Person>? Cleanup(List<Person> lists);
+}
+
+public class DefaultFilter:IBasicFilter
+{
+    public List<Person>? Results { get; set; }
+    public int Counts { get; set; } = 0;
+    public List<Person>? Transform(List<Person> lists)
+    {
+        return lists;
+    }
+
+    public List<Person>? FilterFunc(List<Person> lists)
+    {
+        Results = lists;
+        return lists;
+    }
+
+    public List<Person>? Cleanup(List<Person> lists)
+    {
+        return lists;
+    }
 }
